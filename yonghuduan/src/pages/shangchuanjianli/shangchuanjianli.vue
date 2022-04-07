7e0b448236395e5b41c8a6d2d126049b2e83f641
徐江 <1787297855@qq.com>
2022-04-07 01:38:15 +0000

    修改文件管理页面

--- yonghuduan/src/pages/shangchuanjianli/shangchuanjianli.vue
+++ yonghuduan/src/pages/shangchuanjianli/shangchuanjianli.vue
@@ -1,41 +1,26 @@
 <template>
-  <view class="flex-row page">
+  <view class="flex-col page">
     <NavigationBar class="header" title="文件管理" />
-    <view class="flex-row group_1">
-      <image
-        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16478529400675870778.png"
-        class="image_1"
-      />
+    <view class="flex-row items-center group_1">
+      <image src="@/static/icons/storage.png" class="image_1" />
       <text class="text_1">手机存储</text>
     </view>
     <text class="text_2">分类浏览</text>
-    <view class="flex-row group_2">
-      <image
-        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16478529401392958834.png"
-        class="image_2"
-      />
-      <text class="text_3">图片</text>
+    <view class="flex-row items-center group_2">
+      <image src="@/static/icons/picture.png" class="image_2" />
+      <text class="text_1">图片</text>
     </view>
-    <view class="flex-row group_3">
-      <image
-        src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16478529581461092437.png"
-        class="image_3"
-      />
-      <text class="text_4">音乐</text>
+    <view class="flex-row items-center group_2">
+      <image src="@/static/icons/music.png" class="image_3" />
+      <text class="text_1">音乐</text>
     </view>
-    <view class="flex-row group_4">
-      <image
-        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16478529402558300988.png"
-        class="image_4"
-      />
-      <text class="text_5">视频</text>
+    <view class="flex-row items-center group_2">
+      <image src="@/static/icons/video.png" class="image_3" />
+      <text class="text_1">视频</text>
     </view>
-    <view class="flex-row group_5">
-      <image
-        src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16478529581461465582.png"
-        class="image_5"
-      />
-      <text class="text_6">文档</text>
+    <view class="flex-row items-center group_2">
+      <image src="@/static/icons/document.png" class="image_2" />
+      <text class="text_1">文档</text>
     </view>
   </view>
 </template>
@@ -52,10 +37,8 @@ const imageOnClick = () => {
 
 <style lang="scss" scoped>
 .page {
-  padding-bottom: 634rpx;
-  background-color: rgb(255, 255, 255);
   height: 1334rpx;
-  width: 100%;
+  width: 750rpx;
   overflow-y: auto;
   position: relative;
   .header {
@@ -65,107 +48,48 @@ const imageOnClick = () => {
     top: 0;
   }
   .group_1 {
-    color: rgb(0, 0, 0);
+    width: 710rpx;
+    height: 80rpx;
     font-size: 25rpx;
     line-height: 23rpx;
     white-space: nowrap;
-    position: absolute;
-    left: 40rpx;
-    right: 40rpx;
-    top: 130rpx;
+    margin-top: 130rpx;
+    margin-left: 40rpx;
+    border-bottom: 2rpx solid rgb(230, 230, 230);
     .image_1 {
       border-radius: 10rpx;
       width: 60rpx;
       height: 60rpx;
     }
-    .text_1 {
-      margin-left: 51rpx;
-      align-self: center;
-    }
   }
-  .text_2 {
-    color: rgba(0, 0, 0, 0.8);
-    font-size: 30rpx;
-    line-height: 28rpx;
-    white-space: nowrap;
-    position: absolute;
-    left: 41rpx;
-    top: 233rpx;
-  }
+
   .group_2 {
-    color: rgb(0, 0, 0);
+    width: 710rpx;
+    height: 80rpx;
     font-size: 25rpx;
-    line-height: 23rpx;
     white-space: nowrap;
-    position: absolute;
-    left: 40rpx;
-    right: 40rpx;
-    top: 315rpx;
+    margin-top: 30rpx;
+    margin-left: 40rpx;
+    border-bottom: 2rpx solid rgb(230, 230, 230);
     .image_2 {
       width: 60rpx;
       height: 60rpx;
     }
-    .text_3 {
-      margin-left: 52rpx;
-      align-self: center;
-    }
-  }
-  .group_3 {
-    color: rgb(0, 0, 0);
-    font-size: 25rpx;
-    line-height: 23rpx;
-    white-space: nowrap;
-    position: absolute;
-    left: 40rpx;
-    right: 40rpx;
-    top: 425rpx;
     .image_3 {
-      border-radius: 50rpx;
+      border-radius: 100%;
       width: 60rpx;
       height: 60rpx;
     }
-    .text_4 {
-      margin-left: 51rpx;
-      align-self: center;
-    }
   }
-  .group_4 {
-    color: rgb(0, 0, 0);
-    font-size: 25rpx;
-    line-height: 23rpx;
-    white-space: nowrap;
-    position: absolute;
-    left: 40rpx;
-    right: 40rpx;
-    top: 535rpx;
-    .image_4 {
-      border-radius: 100rpx;
-      width: 60rpx;
-      height: 60rpx;
-    }
-    .text_5 {
-      margin-left: 51rpx;
-      align-self: center;
-    }
+  .text_1 {
+    padding-left: 50rpx;
   }
-  .group_5 {
-    color: rgb(0, 0, 0);
-    font-size: 25rpx;
-    line-height: 23rpx;
+  .text_2 {
+    font-size: 30rpx;
+    line-height: 28rpx;
     white-space: nowrap;
-    position: absolute;
-    left: 40rpx;
-    right: 40rpx;
-    bottom: 634rpx;
-    .image_5 {
-      border-radius: 5rpx;
-      width: 60rpx;
-      height: 50rpx;
-    }
-    .text_6 {
-      margin-left: 51rpx;
-      align-self: center;
-    }
+    margin-top: 20rpx;
+    margin-left: 40rpx;
   }
 }
 </style>
