7af761cadb31ac1229233a632d362a5c5f190458
徐江 <1787297855@qq.com>
2022-04-07 03:58:13 +0000

    用户反馈页面

--- yonghuduan/src/pages/yonghufankui/yonghufankui.vue
+++ yonghuduan/src/pages/yonghufankui/yonghufankui.vue
@@ -1,58 +1,35 @@
 <template>
-  <view class="flex-row page">
+  <view class="flex-col page">
     <NavigationBar class="header" title="用户反馈" />
-    <view class="flex-col group_1">
-      <text class="text_1">请选择反馈类型</text>
-      <view class="flex-row group_2">
-        <view class="flex-col items-center text-wrapper">
-          <text>账号相关</text>
-        </view>
-        <view class="text-wrapper_1 flex-col items-center">
-          <text>推荐相关</text>
-        </view>
-        <view class="text-wrapper_1 flex-col items-center">
-          <text>邀约相关</text>
-        </view>
-        <view class="flex-col items-center text-wrapper_2">
-          <text>简历相关</text>
+    <view class=" user-feedback">
+      <view class="retroaction-type">
+        <text style="font-size: 28rpx;">请选择反馈类型</text>
+        <view class="flex-row retroact-type">
+          <view
+          class="justify-center items-center retroact"
+          :class="choseNum.includes(i)?'active':''"
+          v-for="(retroactionType,i) in retroactionTypes"
+          :key="i"
+          @click="choseType(i)"
+          ><text>{{retroactionType}}</text></view>
         </view>
       </view>
-      <view class="flex-row group_3">
-        <view class="left-text-wrapper flex-col items-center view_2">
-          <text>VIP相关</text>
-        </view>
-        <view class="left-text-wrapper flex-col items-center">
-          <text>教育相关</text>
-        </view>
-        <view class="left-text-wrapper flex-col items-center">
-          <text>其他</text>
+      <view class="flex-col elaborate">
+        <text style="font-size: 28rpx;">详细描述</text>
+        <view class="describe">
+          <scroll-view class="input-set" :scroll-y="true" @scroll="scroll">
+            <textarea  class="input" :placeholder="opinion" maxlength="-1" :auto-height="true"/>
+          </scroll-view>
+          <view class="flex-col items-center justify-center upload">
+            <image class="image" src="@/static/icons/camera.png"/>
+            <text>上传图片</text>
+          </view>
         </view>
       </view>
+      <text class="annotation">最多3张支持JPG、JPEG、PNG格式（选填）</text>
     </view>
-    <view class="flex-col group_4">
-      <view class="flex-col">
-        <text class="text_9">详细描述</text>
-        <view class="flex-col section_1">
-          <view class="flex-col group_6">
-            <text>{{ opinion }}</text>
-            <text>求职者。（不得少于5个字符）</text>
-          </view>
-          <view class="flex-col items-center section_2">
-            <image
-              src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481303750608882885.png"
-              class="image_1"
-            />
-            <text class="text_12">上传图片</text>
-          </view>
-        </view>
-        <text class="text_13">最多3张支持JPG、JPEG、PNG格式（选填）</text>
-      </view>
-      <view class="flex-col items-center button">
-        <image
-          src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481303750154860581.png"
-          class="image_2"
-        />
-      </view>
+    <view class="justify-center items-center sub">
+      <text>提交</text>
     </view>
   </view>
 </template>
@@ -60,165 +37,110 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import NavigationBar from '@/components/NavigationBar/NavigationBar.vue'
-const opinion = ref('请写下您的宝贵意见，东江招聘致力于服务好每一位')
-const imageOnClick = () => {
-  // @ts-ignore
-  uni.navigateBack()
+const opinion = ref('请写下您的宝贵意见，东江招聘致力于服务好每一位求职者（不得少与5个字符）')
+const retroactionTypes = ref([
+  '账号相关',
+  '推荐相关',
+  '邀约相关',
+  '简历相关',
+  '教育相关',
+  '其他'
+])
+
+const choseNum = ref([])
+const choseType = (index: number) => {
+  if (choseNum.value.includes(index)) {
+    choseNum.value.splice(choseNum.value.indexOf(index), 1)
+  }else{
+    choseNum.value.push(index)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.page {
-  background-color: rgb(255, 255, 255);
-  height: 1334rpx;
-  width: 100%;
-  overflow-y: auto;
-  position: relative;
-  .header {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-  }
-  .group_1 {
-    padding: 22rpx 20rpx 21rpx;
-    border-radius: 5rpx;
-    overflow: hidden;
-    height: 200rpx;
-    position: absolute;
-    left: 20rpx;
-    right: 20rpx;
-    top: 110rpx;
-    .text_1 {
-      margin-left: 5rpx;
-      color: rgb(0, 0, 0);
-      font-size: 25rpx;
-      line-height: 23rpx;
-      white-space: nowrap;
-    }
-    .group_2 {
-      margin-top: 35rpx;
-      color: rgb(0, 0, 0);
-      font-size: 22rpx;
-      line-height: 20rpx;
-      white-space: nowrap;
-      .text-wrapper {
-        padding: 7rpx 0 4rpx;
-        flex: 1 1 135rpx;
-        border-radius: 5rpx;
-        overflow: hidden;
-        height: 35rpx;
-        border: solid 2rpx rgb(210, 210, 210);
-      }
-      .text-wrapper_1 {
-        margin-left: 28rpx;
-        flex: 1 1 135rpx;
-        padding: 8rpx 0 3rpx;
-        border-radius: 5rpx;
-        overflow: hidden;
-        height: 35rpx;
-        border: solid 2rpx rgb(210, 210, 210);
-      }
-      .text-wrapper_2 {
-        margin-left: 28rpx;
-        margin-right: 40rpx;
-        padding: 7rpx 0 3rpx;
-        flex: 1 1 135rpx;
-        border-radius: 5rpx;
-        overflow: hidden;
-        height: 35rpx;
-        border: solid 2rpx rgb(210, 210, 210);
-      }
-    }
-    .group_3 {
-      margin-top: 30rpx;
-      color: rgb(0, 0, 0);
-      font-size: 22rpx;
-      line-height: 20rpx;
-      white-space: nowrap;
-      .left-text-wrapper {
-        margin-left: 28rpx;
-        padding: 8rpx 0 3rpx;
-        border-radius: 5rpx;
-        overflow: hidden;
-        width: 135rpx;
-        height: 35rpx;
-        border: solid 2rpx rgb(210, 210, 210);
-      }
-      .view_2 {
-        margin-left: 0;
-      }
-    }
-  }
-  .group_4 {
-    padding: 42rpx 40rpx 50rpx;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    .button {
-      margin-top: 316rpx;
-      padding: 22rpx 0 23rpx;
-      align-self: center;
+  .page {
+    height: 1334rpx;
+    width: 750rpx;
+    overflow-y: auto;
+    position: relative;
+    .active {
       background-color: rgb(35, 193, 158);
-      border-radius: 15rpx;
-      overflow: hidden;
-      width: 550rpx;
-      height: 80rpx;
-      .image_2 {
-        width: 66rpx;
-        height: 35rpx;
+    }
+    .header {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+    }
+    .user-feedback {
+      width: 690rpx;
+      margin-left: 30rpx;
+      margin-top: 130rpx;
+      .retroaction-type {
+          width: 690rpx;
+        .retroact-type {
+          flex-wrap: wrap;
+          .retroact {
+            width: 140rpx;
+            height: 40rpx;
+            font-size: 25rpx;
+            margin-top: 15rpx;
+            margin-right: 30rpx;
+            border: 2rpx solid rgb(230,230,230);
+            border-radius: 5rpx;
+            white-space: normal;
+          }
+        }
       }
-    }
-    .text_9 {
-      color: rgb(0, 0, 0);
-      font-size: 25rpx;
-      line-height: 23rpx;
-      white-space: nowrap;
-    }
-    .section_1 {
-      margin-top: 25rpx;
-      padding: 19rpx 21rpx 24rpx 21rpx;
-      background-color: rgba(229, 229, 229, 0.5);
-      border-radius: 5rpx;
-      overflow: hidden;
-      height: 450rpx;
-      .group_6 {
-        color: rgba(0, 0, 0, 0.5);
+      .elaborate {
         font-size: 25rpx;
-        line-height: 30rpx;
-        white-space: nowrap;
+        width: 690rpx;
+        margin-top: 30rpx;
+        .describe {
+          width: 670rpx;
+          height: 500rpx;
+          margin-top: 25rpx;
+          background-color: rgb(215,215,215);
+          .input-set {
+            width: 660rpx;
+            height: 320rpx;
+            padding-left: 10rpx;
+            overflow: hidden;
+            .input {
+            line-height: 35rpx;
+            }
+          }
+
+          .upload {
+            width: 120rpx;
+            height: 120rpx;
+            margin-top: 40rpx;
+            margin-left: 20rpx;
+            border: 3rpx solid rgb(245, 124, 0);
+            border-radius: 10rpx;
+            .image{
+                width: 35rpx;
+                height: 35rpx;
+              }
+          }
+        }
       }
-      .section_2 {
-        margin-left: 3rpx;
-        margin-top: 221rpx;
-        padding: 24rpx 0 23rpx;
-        color: rgba(0, 0, 0, 0.5);
-        font-size: 18rpx;
-        line-height: 17rpx;
-        white-space: nowrap;
-        border-radius: 5rpx;
-        overflow: hidden;
-        width: 125rpx;
-        height: 125rpx;
-        border: solid 2rpx rgb(245, 124, 0);
-        .image_1 {
-          width: 42rpx;
-          height: 38rpx;
-        }
-        .text_12 {
-          margin-top: 20rpx;
-        }
+      .annotation {
+        font-size: 15rpx;
+        color: gray;
+        margin-top: 15rpx;
       }
     }
-    .text_13 {
-      margin-top: 22rpx;
-      color: rgba(0, 0, 0, 0.5);
-      font-size: 18rpx;
-      line-height: 17rpx;
-      white-space: nowrap;
-      opacity: 0.39;
-    }
+    .sub {
+        width: 630rpx;
+        height: 80rpx;
+        margin-left: 60rpx;
+        position: fixed;
+        bottom: 40rpx;
+        background-color: rgb(35, 193, 158);
+        font-size: 30rpx;
+        border-radius: 10rpx;
+      }
+
   }
-}
 </style>
