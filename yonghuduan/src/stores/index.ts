14f248b76dab3141342657909e109fd95db57086
张旭东 <2219454275@qq.com>
2022-04-10 13:40:46 +0000

    添加vuex持久化

--- yonghuduan/src/stores/index.ts
+++ yonghuduan/src/stores/index.ts
@@ -1,5 +1,6 @@
 import { InjectionKey } from 'vue'
 import { createStore, Store } from 'vuex'
+import VuexPersister from 'vuex-persister'
 
 export interface State {
     systemInfo: Object,
@@ -8,15 +9,35 @@ export interface State {
 
 export const key: InjectionKey<Store<State>> = Symbol()
 
+const vuexPersister = new VuexPersister<State>({
+    storage: {
+        getItem: (key) => uni.getStorageSync(key),
+        setItem: (key, value) => uni.setStorageSync(key, value),
+        removeItem: (key) => uni.removeStorageSync(key),
+        length: uni.getStorageInfoSync().keys.length,
+        clear: () => uni.clearStorageSync(),
+        key: (key: number) => {
+            const keys = uni.getStorageInfoSync().keys
+            return key <= keys.length ? keys[key - 1] : null
+        }
+    }
+})
+
 export const store = createStore<State>({
     state: () => ({
-        systemInfo: uni.getSystemInfoSync(),
-        /* #ifdef MP-WEIXIN || MP-ALIPAY || MP-BAIDU || MP-TOUTIAO || MP-QQ */
-        menuButtonInfo: uni.getMenuButtonBoundingClientRect(),
-        /* #endif */
-
+        systemInfo: {},
+        menuButtonInfo: {},
     }),
+    mutations: {
+        setSystemInfo(state, systemInfo) {
+            state.systemInfo = systemInfo
+        },
+        setMenuButtonInfo(state, menuButtonInfo) {
+            state.menuButtonInfo = menuButtonInfo
+        }
+    },
     modules: {
 
-    }
+    },
+    plugins: [vuexPersister.persist]
 })
