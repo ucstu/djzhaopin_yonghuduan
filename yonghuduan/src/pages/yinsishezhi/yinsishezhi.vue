d65e16cca63558c6f7efbca6c1b16ed415037ca3
徐江 <1787297855@qq.com>
2022-04-10 10:21:30 +0000

    隐私设置

--- yonghuduan/src/pages/yinsishezhi/yinsishezhi.vue
+++ yonghuduan/src/pages/yinsishezhi/yinsishezhi.vue
@@ -1,32 +1,32 @@
 <template>
-  <view class="flex-row page">
+  <view class="flex-col page">
     <NavigationBar class="header" title="隐私设置" />
-    <text class="text_1">匿名展示</text>
-    <view class="justify-between group_1">
-      <view class="flex-row">
-        <image
-          src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481303752534959571.png"
-          class="image_1"
-        />
-        <view class="right-group">
-          <text class="text_2">{{ userName }}</text>
-          <text class="text_3">（实名展示）</text>
-        </view>
+    <view class="group-1">匿名展示</view>
+    <view class="justify-between items-center group-2">
+      <view class="flex-row justify-center items-center group-box">
+        <image src="@/static/icons/heard.png" class="image" />
+        <text style="font-size: 30rpx; padding-left: 15rpx;">{{ userName }}</text>
+        <text style="font-size: 28rpx; padding-left: 15rpx; color: rgba(0, 0, 0, 0.6);">(实名展示)</text>
       </view>
-      <image
-        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481303752635769273.png"
-        class="image_3"
+      <radio
+        style="transform:scale(0.5)"
+        value="1"
+        :checked="realName"
+        @click="anonymous = false; realName = true"
       />
     </view>
-    <view class="flex-row group_3">
-      <image
-        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481303752534959571.png"
-        class="image_1"
-      />
-      <view class="right-group">
-        <text class="text_5">张先生</text>
-        <text class="text_3">（匿名展示）</text>
+    <view class="justify-between items-center group-2">
+      <view class="flex-row justify-center items-center group-box">
+        <image src="@/static/icons/heard.png" class="image" />
+        <text style="font-size: 30rpx; padding-left: 15rpx;">{{ userName[0] }}先生</text>
+        <text style="font-size: 28rpx; padding-left: 15rpx; color: rgba(0, 0, 0, 0.6);">(匿名展示)</text>
       </view>
+      <radio
+        style="transform:scale(0.5)"
+        value="2"
+        :checked="anonymous"
+        @click="realName = false ; anonymous = true"
+      />
     </view>
   </view>
 </template>
@@ -35,79 +35,30 @@
 import { ref } from 'vue';
 import NavigationBar from '@/components/NavigationBar/NavigationBar.vue'
 const userName = ref('张三')
-const imageOnClick = () => {
-  // @ts-ignore
-  uni.navigateBack()
-}
+const realName = ref(true)
+const anonymous = ref(false)
+
 </script>
 
 <style lang="scss" scoped>
 .page {
-  padding-bottom: 979rpx;
-  background-color: rgb(255, 255, 255);
-  height: 1334rpx;
-  width: 100%;
-  overflow-y: auto;
-  position: relative;
-  .header {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
+  .group-1 {
+    font-size: 35rpx;
+    margin-left: 30rpx;
   }
-  .text_1 {
-    color: rgb(0, 0, 0);
-    font-size: 30rpx;
-    line-height: 28rpx;
-    white-space: nowrap;
-    position: absolute;
-    left: 43rpx;
-    top: 148rpx;
-  }
-  .group_1 {
-    position: absolute;
-    left: 35rpx;
-    right: 35rpx;
-    top: 205rpx;
-    .image_3 {
-      margin-right: 17rpx;
-      align-self: center;
-      width: 31rpx;
-      height: 31rpx;
+  .group-2 {
+    width: 710rpx;
+    height: 100rpx;
+    margin-left: 20rpx;
+    margin-top: 20rpx;
+    .group-box {
+      padding-left: 10rpx;
     }
   }
-  .group_3 {
-    position: absolute;
-    left: 35rpx;
-    right: 35rpx;
-    top: 295rpx;
-  }
-  .image_1 {
-    border-radius: 60rpx;
-    width: 60rpx;
-    height: 60rpx;
-  }
-  .right-group {
-    margin-left: 27rpx;
-    align-self: center;
-    line-height: 24rpx;
-    white-space: nowrap;
-    height: 25rpx;
-    .text_5 {
-      color: rgb(0, 0, 0);
-      font-size: 28rpx;
-      line-height: 26rpx;
-    }
-    .text_3 {
-      color: rgba(0, 0, 0, 0.6);
-      font-size: 26rpx;
-      line-height: 25rpx;
-    }
-    .text_2 {
-      color: rgb(0, 0, 0);
-      font-size: 28rpx;
-      line-height: 25rpx;
-    }
+  .image {
+    width: 80rpx;
+    height: 80rpx;
+    border-radius: 100%;
   }
 }
 </style>
