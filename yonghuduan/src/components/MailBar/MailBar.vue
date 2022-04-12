23195851210778a9bffdcb8b3cfa8944da55065b
徐江 <1787297855@qq.com>
2022-04-12 08:52:08 +0000

    消息页面

--- yonghuduan/src/components/MailBar/MailBar.vue
+++ yonghuduan/src/components/MailBar/MailBar.vue
@@ -1,16 +1,15 @@
 <template>
-  <view @click="rootViewOnClick" class="justify-between component">
-    <view class="flex-row group_1">
-      <view class="flex-col items-center group_2">
-        <image class="image" :src="avatarAddress" />
-        <image class="image_1" :src="messageCount" />
+  <view class="flex-row items-center info-box" @click="toChatPage">
+    <image class="image-heard" src="@/static/icons/heard.png" />
+    <view class="infos">
+      <view class="flex-row justify-between name-time">
+        <text>{{ name }}</text>
+        <text style="font-size: 25rpx">{{ time }}</text>
       </view>
-      <view class="flex-col group_3">
-        <text class="text">{{ name }}</text>
-        <text class="text_1">{{ message }}</text>
+      <view class="message">
+        <text>{{ message }}</text>
       </view>
     </view>
-    <text class="text_2">{{ time }}</text>
   </view>
 </template>
 
@@ -18,65 +17,46 @@
 import { ref } from 'vue';
 
 
-      const time=ref( '昨天')
-      const avatarAddress=ref(
-        'https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481301635901980128.png')
-      const messageCount=ref(
-        'https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/623287845a7e3f0310c3a3f7/623446dc62a7d90011023514/16481301674203179834.png')
-      const name=ref( '张三')
-      const message=ref( '方便投递吗？')
+const time = ref('昨天')
+const name = ref('张三')
+const message = ref('方便投递吗卡夫卡吉林省地方卡机黑龙江撒旦立刻反击啊考了几分卡了？')
 
-    const rootViewOnClick=()=> {
-      uni.navigateTo({ url: '/pages/liaotianyemian/liaotianyemian' })
-    }
+const toChatPage = () => {
+  uni.navigateTo({ url: '/pages/liaotianyemian/liaotianyemian' })
+}
 </script>
 
 <style lang="scss" scoped>
-.component {
-  padding-left: 50rpx;
-  padding-right: 48rpx;
-  .group_1 {
-    height: 76rpx;
-    .group_2 {
-      width: 82rpx;
-      position: relative;
-      .image {
-        border-radius: 38rpx;
-        width: 75rpx;
-        height: 75rpx;
-      }
-      .image_1 {
-        border-radius: 13rpx;
-        width: 22rpx;
-        height: 22rpx;
-        position: absolute;
-        right: 0;
-        top: 0;
-      }
-    }
-    .group_3 {
-      margin: 7rpx -36rpx 4rpx 18rpx;
-      .text {
-        color: rgb(0, 0, 0);
-        font-size: 28rpx;
-        line-height: 25rpx;
-        white-space: nowrap;
-      }
-      .text_1 {
-        margin-top: 19rpx;
-        color: rgba(0, 0, 0, 0.5);
-        font-size: 24rpx;
-        line-height: 22rpx;
-        white-space: nowrap;
-      }
-    }
+.info-box {
+  width: 690rpx;
+  height: 130rpx;
+  margin-left: 30rpx;
+
+  .image-heard {
+    width: 100rpx;
+    height: 100rpx;
+    border-radius: 100%;
   }
-  .text_2 {
-    align-self: center;
-    color: rgba(0, 0, 0, 0.5);
-    font-size: 22rpx;
-    line-height: 20rpx;
-    white-space: nowrap;
+
+  .infos {
+    width: 590rpx;
+    height: auto;
+    padding-left: 20rpx;
+    line-height: 40rpx;
+
+    .name-time {
+      width: 100%;
+      height: auto;
+      font-size: 30rpx;
+    }
+
+    .message {
+      width: 550rpx;
+      font-size: 25rpx;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
   }
 }
 </style>
