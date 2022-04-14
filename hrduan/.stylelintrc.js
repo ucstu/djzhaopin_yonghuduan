ee0e54804b8ad3384776915d1fa573236f1cfb3b
zxd2219 <2219454275@qq.com>
2022-04-14 09:43:09 +0800

    剔除hr端

--- hrduan/.stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-standard-scss",
-    "stylelint-config-recommended-vue",
-    "stylelint-config-recess-order",
-    "stylelint-config-prettier",
-  ]
-}
--- hrduan/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { store, key } from './stores'
-createApp(App).use(router).use(store, key).mount('#app')
